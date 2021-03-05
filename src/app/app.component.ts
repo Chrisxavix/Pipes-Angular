1e8dbcf74a06035d08d38263f657802bf4322bac
Chris <chrischillogallid64@outlook.com>
2021-03-04 23:15:04 -0500

    feat: finish pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes-angular';
+  
+  /* CurrencyPipe */
+  precio: number;
+
+  /* DecimalPipe */
+  numeroDecimal: number;
+
+  /* DatePipe */
+  fechaActual: Date;
+
+  /* PercentPipe */
+  numberPercet: number;
+
+  /* Pipe LowerCasePipe, UpperCasePipe y TitleCasePipe */
+  cadena: string;
+
+  /* SlicePipe */
+  arraySlice: string[];
+  constructor() {
+    this.precio = 12.76;
+    this.numeroDecimal = 19.17689834983;
+    this.fechaActual = new Date();
+    /* números ramdom entre 0 y 1 */
+    this.numberPercet = Math.random();
+    this.cadena = 'Christian, el chido'
+    this.arraySlice = ['perro', 'gato', 'conejo', 'loro']
+  }
+
+  ngOnInit() {
+    setInterval(()=> {
+      this.fechaActual = new Date();
+    }, 1000)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs)
 
 @NgModule({
   declarations: [
@@ -10,9 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-Es'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
